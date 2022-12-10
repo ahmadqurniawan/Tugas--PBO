38a39624259fa0adaa4675416db7cf10e7e8e191
Ahmad Qurniawan <101625257+ahmadqurniawan@users.noreply.github.com>
2022-12-10 20:07:42 +0700

    class turunan dari class abstrak motor

--- Motor_Baru.java
+++ Motor_Baru.java
@@ -1,47 +1,10 @@
-public class Motor_Baru extends Motor{
-        int harga;
-        String tipe_motor;
-        String lama_sewa;
+public class Motor_Baru extends Motor {
+    // Harga sewa motor per hari
+    private int hargaPerHari = 30000;
 
-        public Motor_Baru(String id_sewa) {
-            super(id_sewa);
-        }
-
-        @Override
-        public void Motor_Lama() {
-            System.out.println("1. Yamaha Nmax, 5000000/minggu");
-            System.out.println("2. Honda PCX, 6000000/minggu");
-            System.out.println("3. Kawasaki ZX-25R, 10000000/minggu");
-        }
-
-        @Override
-        public int total(int harga, int lama_sewa) {
-            return harga * lama_sewa;
-        }
-
-        public int getHarga() {
-            return harga;
-        }
-
-        public void setHarga(int harga) {
-            this.harga = harga;
-        }
-
-        public String getTipe_motor() {
-            return tipe_motor;
-        }
-
-        public void setTipe_motor(String tipe_motor) {
-            this.tipe_motor = tipe_motor;
-        }
-
-        public String getLama_sewa() {
-            return lama_sewa;
-        }
-
-        public void setLama_sewa(String lama_sewa) {
-            this.lama_sewa = lama_sewa;
-        }
+    // Method untuk menghitung harga sewa motor per hari
+    @Override
+    public int Hargasewa(int lamasewa) {
+        return lamasewa * hargaPerHari;
     }
-
-
+}
--- Motor_Lama.java
+++ Motor_Lama.java
@@ -1,45 +1,10 @@
-public class Motor_Lama extends Motor{
-    int harga;
-    String tipe_motor;
-    String lama_sewa;
+public class Motor_Lama extends Motor {
+    // Harga sewa motor per hari
+    private int hargaPerHari = 10000;
 
-    public Motor_Lama(String id_sewa) {
-        super(id_sewa);
-    }
-
+    // Method untuk menghitung harga sewa motor per hari
     @Override
-    public void Motor_Lama() {
-        System.out.println("1. Suzuki Nex, 500000/minggu");
-        System.out.println("2. Yamaha Vega-R, 700000/minggu");
-        System.out.println("3. Honda Megapro, 1000000/minggu");
-    }
-
-    @Override
-    public int total(int harga, int lama_sewa) {
-        return harga * lama_sewa;
-    }
-
-    public int getHarga() {
-        return harga;
-    }
-
-    public void setHarga(int harga) {
-        this.harga = harga;
-    }
-
-    public String getTipe_motor() {
-        return tipe_motor;
-    }
-
-    public void setTipe_motor(String tipe_motor) {
-        this.tipe_motor = tipe_motor;
-    }
-
-    public String getLama_sewa() {
-        return lama_sewa;
-    }
-
-    public void setLama_sewa(String lama_sewa) {
-        this.lama_sewa = lama_sewa;
+    public int Hargasewa(int lamasewa) {
+        return lamasewa * hargaPerHari;
     }
 }
