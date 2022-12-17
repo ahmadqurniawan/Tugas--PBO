5db599126a3b53aaa75b3e69c7e5b5056a1db6ce
Ahmad Qurniawan <101625257+ahmadqurniawan@users.noreply.github.com>
2022-12-17 19:38:30 +0700

    class turunan abstrak Motor

--- Motor_Baru.java
+++ Motor_Baru.java
@@ -1,10 +1,51 @@
-public class Motor_Baru extends Motor {
-    // Harga sewa motor per hari
-    private int hargaPerHari = 30000;
+import java.util.Scanner;
+public class Motor_Baru extends Motor{
+    Scanner input = new Scanner(System.in);
+    public int id;
+    public int lamasewa;
+    public int pilih;
+    public int harga;
+    public int jumlah;
 
-    // Method untuk menghitung harga sewa motor per hari
     @Override
-    public int Hargasewa(int lamasewa) {
-        return lamasewa * hargaPerHari;
+    public void insertdata() {
+        System.out.print("Pilih Tipe Motor : ");
+        pilih = input.nextInt();
+        if (pilih == 2){
+            System.out.println("Anda Memilih tipe motor Baru");
+            System.out.println("Tipe-tipe motor lama");
+            System.out.println("1. Yamaha Aerox");
+            System.out.println("2. Honda PCX");
+            System.out.println("3. Kawasaki CRF");
+            System.out.println("Silahkan Pilih : ");
+            id = input.nextInt();
+            switch(id){
+                case 1:
+                    harga = 450000;
+                    System.out.println("Anda memilih motor Yamaha Aerox");
+                    System.out.print("input Lama Sewa : ");
+                    lamasewa = input.nextInt();
+                    jumlah = harga*lamasewa;
+                    System.out.println("Total harga sewa : "+jumlah);
+                    break;
+
+                case 2:
+                    harga = 400000;
+                    System.out.println("Anda memilih motor Honda PCX");
+                    System.out.println("input Lama Sewa : ");
+                    lamasewa = input.nextInt();
+                    jumlah = harga*lamasewa;
+                    System.out.println("Total harga sewa : "+jumlah);
+                    break;
+
+                case 3:
+                    harga = 500000;
+                    System.out.println("Anda memilih motor Kawasaki CRF");
+                    System.out.println("input Lama Sewa : ");
+                    lamasewa = input.nextInt();
+                    jumlah = harga*lamasewa;
+                    System.out.println("Total harga sewa : "+jumlah);
+            }
+        }
     }
 }
--- Motor_Lama.java
+++ Motor_Lama.java
@@ -1,10 +1,51 @@
-public class Motor_Lama extends Motor {
-    // Harga sewa motor per hari
-    private int hargaPerHari = 10000;
+import java.util.Scanner;
+public class Motor_Lama extends Motor{
+    Scanner input = new Scanner(System.in);
+    public int id;
+    public int lamasewa;
+    public int pilih;
+    public int harga;
+    public int jumlah;
 
-    // Method untuk menghitung harga sewa motor per hari
     @Override
-    public int Hargasewa(int lamasewa) {
-        return lamasewa * hargaPerHari;
+    public void insertdata() {
+        System.out.print("Pilih Tipe Motor : ");
+        pilih = input.nextInt();
+        if (pilih == 1){
+            System.out.println("Anda Memilih tipe motor lama");
+            System.out.println("Tipe-tipe motor lama");
+            System.out.println("1. Yamaha Vega R");
+            System.out.println("2. Honda Beat");
+            System.out.println("3. Suzuki Nex");
+            System.out.println("Silahkan Pilih : ");
+            id = input.nextInt();
+            switch(id){
+                case 1:
+                    harga = 250000;
+                    System.out.println("Anda memilih motor Yamaha Vega R");
+                    System.out.print("input Lama Sewa : ");
+                    lamasewa = input.nextInt();
+                    jumlah = harga*lamasewa;
+                    System.out.println("Total harga sewa : "+jumlah);
+                    break;
+
+                case 2:
+                    harga = 300000;
+                    System.out.println("Anda memilih motor Honda Beat");
+                    System.out.print("input Lama Sewa : ");
+                    lamasewa = input.nextInt();
+                    jumlah = harga*lamasewa;
+                    System.out.println("Total harga sewa : "+jumlah);
+                    break;
+
+                case 3:
+                    harga = 200000;
+                    System.out.println("Anda memilih motor Suzuki Nex");
+                    System.out.print("input Lama Sewa : ");
+                    lamasewa = input.nextInt();
+                    jumlah = harga*lamasewa;
+                    System.out.println("Total harga sewa : "+jumlah);
+            }
+        }
     }
 }
