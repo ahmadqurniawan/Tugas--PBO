e2d947631122aaf9c5d6f4c71f8b08b40337f620
Ahmad Qurniawan <101625257+ahmadqurniawan@users.noreply.github.com>
2022-12-24 20:45:42 +0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,17 +1,20 @@
+import java.io.*;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
         PemilikSewa pemilik = new PemilikSewa();
         Scanner PemilikSewa = new Scanner(System.in);
-        System.out.println("PETUGAS");
+        System.out.println("===PETUGAS===");
         System.out.print("Masukkan Nama  : ");
         pemilik.nama = PemilikSewa.nextLine();
         System.out.print("Masukkan NO HP : ");
         pemilik.no_tlpn = PemilikSewa.nextInt();
 
         Penyewa penyewa1 = new Penyewa();
-        Scanner penyewa = new Scanner(System.in);
-        System.out.println("PENYEWA");
+        Scanner penyewa  = new Scanner(System.in);
+        System.out.println("===PENYEWA===");
         System.out.print("Masukkan Nama   : ");
         penyewa1.nama = penyewa.nextLine();
         System.out.print("Masukkan Alamat : ");
@@ -21,24 +24,54 @@ public class Main {
         System.out.print("Masukkan NO.HP  : ");
         penyewa1.no_tlpn = penyewa.nextInt();
 
+        BufferedReader baca =new BufferedReader(new InputStreamReader(System.in));
+        File file = new File("struk.txt");
+        try {
+            Motor_Lama lama = new Motor_Lama();
+            lama.Tipemotor();
+            lama.insertdata();
 
-        System.out.println("===========DATA PETUGAS===========");
-        System.out.println("Nama Petugas : " + pemilik.getNama());
-        System.out.println("No.Hp        : " + pemilik.getNo_tlpn());
+            Motor_Baru baru = new Motor_Baru();
+            baru.Tipemotor();
+            baru.insertdata();
 
-        System.out.println("==========DATA PENYEWA============");
-        System.out.println("Nama Penyewa :" + penyewa1.getNama());
-        System.out.println("No ID        : " + penyewa1.getId_penyewa());
-        System.out.println("No HP        : " + penyewa1.getNo_tlpn());
-        System.out.println("Alamat       : " + penyewa1.getAlamat());
+            PrintWriter output = new PrintWriter(file);
+            System.out.println("===========DATA PETUGAS===========");
+            System.out.println("Nama Petugas : " + pemilik.getNama());
+            System.out.println("No.Hp        : " + pemilik.getNo_tlpn());
+            System.out.println("==================================\n");
 
-        Motor_Lama lama = new Motor_Lama();
-        lama.Tipemotor();
-        lama.insertdata();
+            System.out.println("==========DATA PENYEWA============");
+            System.out.println("Nama Penyewa : " + penyewa1.getNama());
+            System.out.println("No ID        : " + penyewa1.getId_penyewa());
+            System.out.println("No HP        : " + penyewa1.getNo_tlpn());
+            System.out.println("Alamat       : " + penyewa1.getAlamat());
+            System.out.println("==================================\n");
 
-        Motor_Baru baru = new Motor_Baru();
-        baru.Tipemotor();
-        baru.insertdata();
+            System.out.println("============STRUK PENYEWAAN MOTOR============");
+            System.out.println("Nama Penyewa : "+penyewa1.getNama());
+            System.out.println("No.ID        : "+penyewa1.getId_penyewa());
+            System.out.println("NO.HP        : "+penyewa1.getNo_tlpn());
+            System.out.println("Alamat       : "+penyewa1.getAlamat());
+            System.out.println("Total Harga  : "+baru.jumlah);
+            System.out.println("==============================================");
+            System.out.println("=======Terima Kasih Atas Kunjungan Anda=======");
+            System.out.println("==============================================");
 
+            output.println("============STRUK PENYEWAAN MOTOR============");
+            output.println("Nama Penyewa : "+penyewa1.getNama());
+            output.println("No.ID        : "+penyewa1.getId_penyewa());
+            output.println("NO.HP        : "+penyewa1.getNo_tlpn());
+            output.println("Alamat       : "+penyewa1.getAlamat());
+            output.println("Total Harga  : "+baru.jumlah);
+            output.println("==============================================");
+            output.println("=======Terima Kasih Atas Kunjungan Anda=======");
+            output.println("==============================================");
+
+            output.close();
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("File tidak di temukan");
+        }
     }
 }
